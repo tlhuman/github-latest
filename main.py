50cda06cd041e61e586ae4a118076389eb9bb731
tim <lurveyt@uw.edu>
2021-02-06 19:11:27 -0800

    init

--- main.py
+++ main.py
@@ -23,4 +23,10 @@ if __name__ == "__main__":
 
     print(f"{username} event at {created_at}")
 
-
+## INSTRUCTOR SOLUTION
+# username = sys.argv[1]
+#
+# response = requests.get("https://api.github.com/users/{}/events".format(username))
+# events = json.loads(response.content)
+#
+# print(events[0]['created_at'])
